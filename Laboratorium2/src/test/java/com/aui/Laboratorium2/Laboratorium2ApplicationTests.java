386fcc37a353a31fc3b89cdab694fe0a25e5c477
Tysia <tyska.dabrowska@gmail.com>
2023-10-30 14:29:15 +0100

    spring-boot Laboratorium 2 start

---
+++ Laboratorium2/src/test/java/com/aui/Laboratorium2/Laboratorium2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aui.Laboratorium2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorium2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
